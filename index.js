9e4621cea3aa3b1ad70bb6126d063de929a03b71
Noah Reece <noah.w.reece@gmail.com>
2022-02-22 20:26:18 -0500

    commit tree 304af6f0db9e620cb7ba3c49b45dc0673d642156

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+  if (num <= 400) {
+    return "This one is on me!";
+  } else if (num > 2500) {
+    return "No can do.";
+  } else if (num > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(amount){
+  if (amount === 'generous') {
+    return 'Thank you so much.';
+  } else if (amount === 'not as generous') {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
